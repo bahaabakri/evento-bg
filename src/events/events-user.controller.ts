4e2cb485d998178a789f31d7c68e7843a72dab5e
bahaabakri <bahaa.bakri1995@gmail.com>
2025-09-24 10:42:19 +0400

    fix(Events): fix issue in get user by id in events user

--- src/events/events-user.controller.ts
+++ src/events/events-user.controller.ts
@@ -3,18 +3,20 @@ import { EventsService } from './events.service';
 import Serialize from 'src/decorators/serialize.decorator';
 import { EventResponseDto } from './dto/response/event-response.dto';
 import SearchEventDto from './dto/request/search-event.dto';
-
-@Serialize(EventResponseDto)
+import { PaginatedEventsDto } from './dto/response/paginated-events.dto';
+import { EventDto } from './dto/response/event.dto';
 @Controller('events')
 export class EventsUserController {
     constructor(private readonly eventsService: EventsService) {}
     /////////////////// public apis for get events and event by id apis /////////////////
+    @Serialize(PaginatedEventsDto)
     @Get()
     async getEvents(@Query() query:SearchEventDto) {
         const events = await this.eventsService.getEvents(query);
         return events;
     }
 
+    @Serialize(EventDto)
     @Get(':id')
     async getEventById(@Param('id') id: number) {
         const event = await this.eventsService.getEventById(id);
