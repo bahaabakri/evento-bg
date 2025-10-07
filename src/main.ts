98e106b62a5d585abf19a437fb0f1e6877c72a81
Bahaa <bahaa.bakri1995@gmail.com>
2025-10-07 17:58:23 +0400

    chore: work with swigger to make doc

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as express from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // Configure static file serving using express.static
   const staticFilesPath = join(process.cwd(), 'uploads');
   app.use('/uploads', express.static(staticFilesPath));
+
+  // Configure Swagger Doc
+    const config = new DocumentBuilder()
+    .setTitle('Evento API')
+    .setDescription('This is the main documentation for eveto api')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/modules/events/dto/request/create-event.dto.ts
+++ src/modules/events/dto/request/create-event.dto.ts
@@ -1,28 +1,44 @@
-import { IsBoolean, IsISO8601, IsLatitude, IsLongitude, IsNumber, IsString, Length } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsISO8601,
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Length,
+} from 'class-validator';
 
 export default class CreateEventDto {
-    @IsString()
-    @Length(3, 255)
-    name: string;
+  @IsString()
+  @Length(3, 255)
+  @ApiProperty({ description: 'Name of the event' })
+  name: string;
 
-    @IsString()
-    @Length(5, 255)
-    description: string;
+  @IsString()
+  @Length(5, 255)
+  @ApiProperty({ description: 'Event description' })
+  description: string;
 
-    @IsString()
-    @IsISO8601()
-    date: string;
-    
-    @IsString()
-    @Length(3, 255)
-    location: string;
+  @IsString()
+  @IsISO8601()
+  @ApiProperty({ description: 'Date of the event in ISO format' })
+  date: string;
 
-    @IsLongitude()
-    lng:number;
+  @IsString()
+  @Length(3, 255)
+  @ApiProperty({ description: 'The location of the event' })
+  location: string;
 
-    @IsLatitude()
-    lat: number;
+  @IsLongitude()
+  @ApiProperty({ description: 'Longutite' })
+  lng: number;
 
-    @IsString({each: true})
-    imagesIds: string[];
+  @IsLatitude()
+  @ApiProperty({ description: 'Latituite' })
+  lat: number;
+
+  @IsString({ each: true })
+  @ApiProperty({ description: 'Array of images Ids' })
+  imagesIds: string[];
 }
